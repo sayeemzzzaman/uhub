28fed4ffe91e5c5d6161937b12ef1d1831eba152
Salman Saad <devsalmansaad@gmail.com>
2023-08-04 16:42:18 +0600

    route-edit

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('library-books');
 });
-Route::get('/sign-in', function () {
-    return view('sign-in');
-});
+// Route::get('/sign-in', function () {
+//     return view('sign-in');
+// });
 Route::get('/club-archive', function () {
     return view('club-archive');
 });
 
+// middleware codes
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
