9318c80a6b0bb21baeec917eae3eb53fc8951a8a
Salman Saad <devsalmansaad@gmail.com>
2023-08-06 21:24:11 +0600

    user table fixed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('uiuid')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->enum('role', ['admin','student','faculty','librarian'])->default('student');
+            $table->enum('status', ['active','inactive'])->default('active');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
